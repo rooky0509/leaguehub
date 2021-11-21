561690a65c876c509e735248612d41f6a7ffd9a3
rooky0509 <rooky0509@gmail.com>
2021-11-21 20:08:31 +0900

    champ table

--- script.js
+++ script.js
@@ -17,50 +17,119 @@ var champs = [];
 // player icon : https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/profile-icons/
 // stats : https://raw.communitydragon.org/pbe/plugins/rcp-be-lol-game-data/global/default/v1/perk-images/statmods/
 // rune : https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/perk-images/styles/
-
+// d : https://raw.communitydragon.org/latest/game/ux/fonts.bin.json
 
 GET("https://ddragon.leagueoflegends.com/api/versions.json",(versions_data) =>{
-    version = versions_data[0];
-    console.log(version);
-    GET("https://ddragon.leagueoflegends.com/cdn/"+version+"/data/ko_KR/item.json",(items_json) =>{
-      item_data = items_json["data"]
-      items = []
-      i = 0;
-      for (x in item_data){
-        items.push({"name":item_data[x]["name"],"id":x,"data":item_data[x],"index":i++});
+  version = versions_data[0];
+  console.log(version);
+  GET("https://ddragon.leagueoflegends.com/cdn/"+version+"/data/ko_KR/item.json",(items_json) =>{
+
+    item_data = items_json["data"]
+    items = []
+    i = 0;
+    for (x in item_data){
+      items.push({"name":item_data[x]["name"],"id":x,"data":item_data[x],"index":i++});
+    }
+    console.log(items);
+
+    GET("https://raw.communitydragon.org/latest/game/ux/fonts.bin.json",(css_colors_json) =>{
+      css_colors_data = css_colors_json["{9c87124a}"]["styles"]
+      css_colors = {};
+      for (x in css_colors_data){
+        css_color = css_colors_data[x]["color"]; // 0 1 2 3
+        if (css_color!=undefined){
+          css_colors[x.toLowerCase()] = "rgba("+[css_color[2],css_color[1],css_color[0],css_color[3]].join(',')+")";
+        }
       }
-      console.log(items);
-    GET("https://ddragon.leagueoflegends.com/cdn/"+version+"/data/ko_KR/champion.json",(champs_json) =>{
-      champ_data = champs_json["data"]
-      champs = []
-      i = 0;
-      for (x in champ_data){
-        champs.push({"name":champ_data[x]["name"],"id":x,"data":champ_data[x],"index":i++});
-      }
-    
+
+
+      GET("https://ddragon.leagueoflegends.com/cdn/"+version+"/data/ko_KR/champion.json",(champs_json) =>{
+        champ_data = champs_json["data"]
+        champs = []
+        i = 0;
+        for (x in champ_data){
+          champs.push({"name":champ_data[x]["name"],"id":x,"data":champ_data[x],"index":i++});
+        }
+      });
+
+
     });
-    });
+  });
 });
 
-function set(itemIndex){
-  item = items[itemIndex];
-  console.log(itemIndex);
-  document.getElementById("item").src = "http://ddragon.leagueoflegends.com/cdn/"+version+"/img/item/"+item["id"]+".png"
-  document.getElementsByTagName("name")[0].innerHTML = item["data"]["name"];
-  document.getElementsByTagName("gold")[0].innerHTML = item["data"]["gold"]["total"];
-  document.getElementsByTagName("description")[0].innerHTML = item["data"]["description"];
+function set(Index,type){
+  console.log(Index);
+  console.log(type);
+  switch (type){
+    case "item":
+      item = items[Index];
 
+      table = document.getElementById("item_table");
+      
+      table.getElementsByTagName("img")[0].src = "http://ddragon.leagueoflegends.com/cdn/"+version+"/img/item/"+item["id"]+".png";
+      table.getElementsByTagName("img")[0].alt = item["name"]+".png"
+      table.getElementsByTagName("img")[0].title = item["name"]+"\n\n"+item["data"]["plaintext"];
+
+      table.getElementsByTagName("name")[0].innerHTML = item["name"];
+      table.getElementsByTagName("gold")[0].innerHTML = item["data"]["gold"]["total"];
+
+      table.getElementsByTagName("description")[0].innerHTML = item["data"]["description"];
+      Array.from( table.getElementsByTagName("maintext")[0].children ).forEach(child_P => Array.from( child_P.children ).forEach(child => {
+        child.style.color = css_colors[child.nodeName.toLowerCase()]
+      }))
+
+      break;
+    case "champ":
+      champ = champs[Index];
+
+      table = document.getElementById("champ_table");
+      
+      table.getElementsByTagName("img")[0].src = "http://ddragon.leagueoflegends.com/cdn/"+version+"/img/champion/"+champ["id"]+".png";
+      table.getElementsByTagName("img")[0].alt = champ["name"]+".png"
+      table.getElementsByTagName("img")[0].title = champ["name"]+"\n\n"+champ["data"]["plaintext"];
+
+      table.getElementsByTagName("name")[0].innerHTML = champ["name"];
+      /*
+      table.getElementsByTagName("gold").innerHTML = champ["data"]["gold"]["total"];
+
+      table.getElementsByTagName("description").innerHTML = champ["data"]["description"];
+      Array.from( table.getElementsByTagName("maintext").children ).forEach(child_P => Array.from( child_P.children ).forEach(child => {
+        child.style.color = css_colors[child.nodeName.toLowerCase()]
+      }))
+      */
+      break;
+    default:
+      console.log("set function tpye Error");
+  }
+  
+  
   document.getElementsByTagName("html")[0].style.backgroundImage = "url('http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Pyke_0.jpg')";
 }
 
-function search(input){  
+function search(input,type){  
+  console.log(input);
+  console.log(type);
   if(input.length >= 1){
     console.log(input);
-    findItem = items.filter(a => a["name"].indexOf(input) != -1);
-    console.log(findItem);
-    if(findItem[0] != undefined) set(findItem[0]["index"]);
-    // && document.getElementsByTagName("name")[0].innerHTML != find
+    switch (type){
+      case "item":
+        findItem = items.filter(a => a["name"].indexOf(input) != -1);
+        console.log(findItem);
+        if(findItem[0] != undefined) set(findItem[0]["index"],type);
+        // && document.getElementsByTagName("name")[0].innerHTML != find
+        break;
+      case "champ":
+        findChamp = champs.filter(a => a["name"].indexOf(input) != -1);
+        console.log(findChamp);
+        if(findChamp[0] != undefined) set(findChamp[0]["index"],type);
+        // && document.getElementsByTagName("name")[0].innerHTML != find
+        break;
+      default:
+        console.log("search function tpye Error");
+    }
   }
+  
+
 } 
 
 /*
@@ -91,3 +160,4 @@ function search(input){
   git branch                 [현재 브런치 확인]
   git branch -m master main    ㄴ[master -> main]
 */
+
